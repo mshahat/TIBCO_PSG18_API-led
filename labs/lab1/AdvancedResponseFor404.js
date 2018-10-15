5ec9a930f6ac2ecf05e7aba759d73abb6e6de506
Mohamed Shahat <mshahat@computer.org>
2018-10-16 00:14:33 +0100

    general cleanup and restructure - formatting of all documents - swagger as YAML - bwce source code under source

--- labs/lab1/AdvancedResponseFor404.js
+++ labs/lab1/AdvancedResponseFor404.js
@@ -1,51 +1,47 @@
 function customCode(req, res) {
-  /** 
-   * JavaScript code to set mock response,
-   * please refer to documentation for supported API operations. 
-   * res.status = 200
-   * res.body = {name:"cat"} 
-   */
- 
- //your code goes here
- 
- var pathparams = req.params;
+	/** 
+	 * JavaScript code to set mock response,
+	 * please refer to documentation for supported API operations. 
+	 * res.status = 200
+	 * res.body = {name:"cat"} 
+	 */
 
-if (pathparams.customerid==='999')
-{
-	res.status = 404;
-	res.body = {
-  			"errorCode": "CUST_SVC_API_002",
+	//your code goes here
+
+	var pathparams = req.params;
+
+	if (pathparams.customerid === '999') {
+		res.status = 404;
+		res.body = {
+			"errorCode": "CUST_SVC_API_002",
 			"errorID": "6bd9c7af-edeb-4377-bdbe-6f5b3697c7ba",
 			"errorType": "TECHNICAL",
-  			"more_info": "http://loalhost/docs/messages/CUST_SVC_API_000",
-  			"status": 401,
- 			"context": [
-    			{
-      				"name": "optional_name",
-      				"value": "optional_value"
-    			}
-  			],
+			"more_info": "http://loalhost/docs/messages/CUST_SVC_API_000",
+			"status": 401,
+			"context": [
+				{
+					"name": "optional_name",
+					"value": "optional_value"
+				}
+			],
 
-			}
+		}
 
-}
+	}
 
-else{
-  res.status = 200
-  res.body = {
- 			"Address": "3 Silicone alley",
-  			"City": "Palo Alto",
-  			"CreditLimit": 12000000,
-  			"CustID": 123,
-  			"Name": "Moonlit Semiconductors",
-  			"Phone": "(949)202-8025",
-  			"State": "CA",
-  			"zip": "92614"
-			}
-  }
+	else {
+		res.status = 200
+		res.body = {
+			"Address": "3 Silicone alley",
+			"City": "Palo Alto",
+			"CreditLimit": 12000000,
+			"CustID": 123,
+			"Name": "Moonlit Semiconductors",
+			"Phone": "(949)202-8025",
+			"State": "CA",
+			"zip": "92614"
+		}
+	}
 
- 
-
-
-  return res;
+	return res;
 }
